0655addb62ec8cd81204bce1e0b87148c0eea38c
Eco_Min <guldor10.b@gmail.com>
2022-02-05 22:22:39 +0900

    test

--- src/min/Main.java
+++ src/min/Main.java
@@ -3,6 +3,7 @@ package min;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("asdasd");
+        String testMin = "hello java study";
+        System.out.println("testMin = " + testMin);
     }
 }
